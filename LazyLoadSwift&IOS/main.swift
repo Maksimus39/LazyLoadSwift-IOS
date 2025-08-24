09c05809fdeb1328d2b500799860f544826e423c
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-24 14:27:32 -0700

    var let learn

--- LazyLoadSwift&IOS/main.swift
+++ LazyLoadSwift&IOS/main.swift
@@ -1,11 +1,22 @@
+//// 9 - переменные константы и операторы присваивания
 //
-//  main.swift
-//  LazyLoadSwift&IOS
+//let a : Int
+//let b : Double
+//let c : Float
+//let d : String
+//let e : Bool
+//let f : Character
 //
-//  Created by Максим Минаков on 12.08.2025.
 //
-
-import Foundation
-
-print("Hello, World!")
-
+//// Принято писать переменные в верблюжем стиле camelCase
+//
+//let userAge : Int
+//userAge = 2            // в константах не принято менять значение, оператор это не позволит
+//print(userAge)
+//
+//
+//let userName : String = "Maksim"     // Инициализация и присвоение
+//
+//
+//print(userAge, userName)
+//
