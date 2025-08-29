0cb828b6835a1d1de4ff8d8431d725a7954c961c
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-28 21:55:02 -0700

    пционалные значения

--- LazyLoadSwift&IOS/main.swift
+++ LazyLoadSwift&IOS/main.swift
@@ -55,8 +55,76 @@
 
 // 12 - Чтение информации из консоли
 
-print("Введите информацию в консоль:")
-let stringInputConsole = readLine()!
-print(stringInputConsole)
+//print("Введите информацию в консоль:")
+//let stringInputConsole = readLine()!
+//print(stringInputConsole)
 
 
+// 13 Опционалные значения
+
+//var age : Int? = 0
+//age = 1
+//age = 10
+//age = nil
+//
+//print(age ?? 10)
+
+
+//var a : Int = 1
+//var b : Int? = 1
+//
+//print(a == b)      // true а вот наоборот нельзя, в обычную переменную нельзя присваивать опциональный тип и математические операции нельзя делать
+//
+//let c : Int? = a   // 1
+//print(c!)
+
+
+// Извлечение опционала
+
+// 1
+
+//let aa : Int = 1
+//let bb : Int? = 1
+//let cc : Int? = nil
+//
+//if let bb  {
+//    print("aa:", aa + bb)
+//} else {
+//    print("No")
+//}
+
+
+//if let usualCC = cc {
+//    print("cc:", aa + usualCC)
+//} else {
+//    print( "No")
+//}
+
+
+// Небезопасное явное извлечение
+
+//print(aa + bb!)                // ставится восклицательный знак, проверки не происходят в данном случае
+//print(aa + сс!)                // а так не скомпилируется
+
+
+// Неявная небезопасная распаковка
+
+//let num : Int = 1
+//let num2 : Int! = 1
+//let num3 : Int! = nil
+
+//print(num + num2)
+//print(num2 + num3!)
+
+
+
+
+// Значение по умолчанию
+
+//let result : Int?? = 42
+//
+//if let res2 = result {
+//    print("res2:", res2!)
+//}
+//
+//print(result! ?? 0)
